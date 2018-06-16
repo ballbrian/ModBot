c0037e9ae0b7786b1b008ea707fabdfd11c822e7
Brian Ball <ballb1221@live.com>
2018-06-16 18:48:04 -0500

    Added Bets Duty - https://trello.com/c/lLL7SdyC Updated Tests with PR Duty and Bets Duty

---
+++ ModBot.Core/Commands/BetsDutyCommandSet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using ModBot.Core.Services;
+using Mod_Bot.Commands;
+
+namespace ModBot.Core.Commands
+{
+    [Group("Bets")]
+    public class BetsDutyCommandSet : ModuleBase<SocketCommandContext>, IModCommandSet
+    {
+        private readonly ICommandReplyService _commandReplyService;
+
+        public ModType ModType => ModType.Bets;
+
+        public BetsDutyCommandSet(ICommandReplyService commandReplyService)
+        {
+            _commandReplyService = commandReplyService;
+        }
+
+        [Command("add")]
+        public async Task HandleAddCommand(IGuildUser socketGuildUser)
+        {
+            await ReplyAsync(_commandReplyService.AddModResponse(ModType, socketGuildUser));
+        }
+
+        [Command("remove")]
+        public async Task HandleRemoveCommand(IGuildUser socketGuildUser)
+        {
+            await ReplyAsync(_commandReplyService.RemoveModResponse(ModType, socketGuildUser));
+        }
+
+        [Command]
+        public async Task HandleDisplayCommand()
+        {
+            await ReplyAsync(_commandReplyService.DisplayModsResponse(ModType));
+        }
+
+        [Command("purge")]
+        public async Task HandlePurgeCommand()
+        {
+            await ReplyAsync(_commandReplyService.PurgeModsResponse(ModType));
+        }
+    }
+}
